78d32150e83b1b137dc4d23b1ab575213b740e54
Jesus G <113757515+py3user22@users.noreply.github.com>
2024-10-07 18:55:30 -0500

    save demo9 save, almost got the final

--- myapp1002/templates/1007_kaggle_list_demo9.py
+++ myapp1002/templates/1007_kaggle_list_demo9.py
@@ -25,6 +25,20 @@ def fashionably_late(arrivals, name):
     if len(arrivals) <= 2:
         return True
 
+    """if odd list -> the middle element is not included & should be false"""
+    # if len(arrivals) % 3 == 0:
+    # mid_pnt2 = [arrivals[half_pnt - 1], arrivals[half_pnt])
+    # if name in half_non:
+    #     return False
+    # elif name in mid_pnt2:
+    #     return True
+    # else:
+    #     pass
+
+    """if even list -> the two middle elements are included & should be true"""
+    # if len(arrivals) % 2 == 0:
+    #
+
     if name in fir_sec:
         return False
     elif name in las_ppl:
@@ -39,15 +53,15 @@ def fashionably_late(arrivals, name):
 list_one = ['Adela', 'Fleda', 'Owen', 'May', 'Mona', 'Gilbert', 'Ford']
 list_two = ['Paul', 'John', 'Ringo', 'George']
 list_tt = ['Alice', 'Bob', 'Charlie', 'David']
-list_small = ['May', 'April']
+list_small = ['June', 'April']
 
 hh = fashionably_late(list_one, 'Gilbert')
-print(hh)
+print(hh)  # T
 nn = fashionably_late(list_two, 'Ringo')
-print(nn)
+print(nn)  # F --> should be True
 bb = fashionably_late(list_one, 'Fleda')
-print(bb)
+print(bb)  # F
 cc = fashionably_late(list_small, 'April')
-print(cc)
+print(cc)  # T
 dd = fashionably_late(list_one, 'May')
-print(dd)  # should be False
+print(dd)  # F --> should be False
