d35f0d93cba529ac579b40e0aa2edf64657d8241
Jesus G <113757515+py3user22@users.noreply.github.com>
2024-10-27 22:42:13 -0500

    1027 python project demo3

--- myapp1002/templates/1027_py3_practice_demo2.py
+++ myapp1002/templates/1027_py3_practice_demo2.py
@@ -47,27 +47,49 @@ print("----------------------------\n")
 
 
 def days_between(year1: int, month1: int, day1: int, year2: int, month2: int, day2: int) -> int:
+
+    # define local variables
     valid_check1 = date_is_valid(year1, month1, day1)
     valid_check2 = date_is_valid(year2, month2, day2)
-    if valid_check1 and valid_check2:
-        print("your checked out and dates are valid")
-        return datetime.timedelta(valid_check2 - valid_check1)
-    else:
-        print("your dates are not working, due to invalid dates input")
-        return 0
+    # create datetime objects
+    date1 = datetime.date(year1, month1, day1)
+    date2 = datetime.date(year2, month2, day2)
+
+    # if statement to check if valid inputs & prints out valid msg
+    if not (valid_check1 and valid_check2):
+        return "0 - Invalid dates provided"
+
+    print(date1, date2)   # 2000-01-18 2024-10-27
+
+    year_diff = date2 - date1
+    print("year difference in days from year2 to year1: ")
+    return (year_diff.days)
 
 
 
 
 
-days1 = days_between(2024, 10, 27, 2024, 10, 27)
+
+
+
+
+
+
+
+
+
+days1 = days_between(2022, 10, 27, 2024, 10, 27)
 print(days1)  # your checked out and dates are working
 print("________")
-days2 = days_between(2024, 10, 27, 2024, 10, 35)
+
+print("________")
+days3 = days_between(2000, 1, 18, 2024, 10, 27)
+print(days3)
+
+print("________")
+
+days2 = days_between(1998, 2, 15, 2024, 10, 27)
 print(days2)  # 0
-
-
-
 
 
 
